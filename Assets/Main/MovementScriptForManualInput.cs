76383ca8c98eb89c510112ea08abc46c5735b11e
LukasBaliukonis567 <72207017+LukasBaliukonis567@users.noreply.github.com>
2025-07-17 20:46:45 +0300

    intial character setup
    Added a movable character to control
    Added basic XP system
    Added xp  Orb spawning logic

---
+++ Assets/Main/MovementScriptForManualInput.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MovementScript : MonoBehaviour
+{
+    public const float movementSpeed = 5.0f;
+    public const float diagononalSpeed = 3.5f;
+    private float pointsPickedUp = 0.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.gravityScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
+        BoxCollider2D boxCollider2D = GetComponent<BoxCollider2D>();
+
+
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            rigidbody2D.position += (Vector2.up + Vector2.right) * Time.deltaTime * diagononalSpeed;
+        }
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            rigidbody2D.position += (Vector2.up + Vector2.left) * Time.deltaTime * diagononalSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            rigidbody2D.position += (Vector2.down + Vector2.right) * Time.deltaTime * diagononalSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            rigidbody2D.position += (Vector2.down + Vector2.left) * Time.deltaTime * diagononalSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody2D.position += Vector2.up * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody2D.position += Vector2.down * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody2D.position += Vector2.left * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody2D.position += Vector2.right * Time.deltaTime * movementSpeed;
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Overlapping with: " + other.gameObject.name);
+        SphereSpawner sphere = other.GetComponent<SphereSpawner>();
+        if (sphere != null)
+        {
+            pointsPickedUp += sphere.XPValue;
+            Debug.Log($"Trigger overlap! XP: {sphere.XPValue}");
+            Destroy(other.gameObject);
+            Debug.Log($"Total points picked up: {pointsPickedUp}");
+        }
+    }
+}
